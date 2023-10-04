ad2c79f575a31f44731832febdffed36d5d4f303
shylxsh <shyleshshankar22@gmail.com>
2023-10-04 17:11:08 +0530

    Updates

---
+++ precache-manifest.d71faea396a096d28fa5269daeb8b875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1507b5e46c74db11b48447960ce493f3",
+    "url": "/firegram-final-files/index.html"
+  },
+  {
+    "revision": "f3889862ab903d42a92d",
+    "url": "/firegram-final-files/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "1904939ca21b2dab421f",
+    "url": "/firegram-final-files/static/js/2.598f0e9c.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/firegram-final-files/static/js/2.598f0e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3889862ab903d42a92d",
+    "url": "/firegram-final-files/static/js/main.0a1c143f.chunk.js"
+  },
+  {
+    "revision": "57f54327c29f22aee859",
+    "url": "/firegram-final-files/static/js/runtime-main.ec733a86.js"
+  }
+]);
